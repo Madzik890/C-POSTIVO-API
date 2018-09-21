ba4f2a3b98a4812ca6a50ea9f832ce2f843c94b2
madzik890 <Madzik890@hotmail.com>
2018-09-21 13:44:53 +0200

    Remove functions which now are in 'stringMan'

--- src/recipient.c
+++ src/recipient.c
@@ -1,15 +1,8 @@
 #include "recipient.h"
-#include <stdio.h>
-#include <string.h>
-
-#define STRING_SIZE 255
-
-void assingSource(const unsigned int answer, char ** stringOut);
-void assingString(const char * consoleWrite, char ** stringOut);
-void removeNewLine(char* string);
+#include "stringMan.h"
 
 /// <summary>
-/// Creats the recipient from user inputs.
+/// Creates the recipient from user inputs.
 /// </summary>
 /// <param name = "recipient"> Pointer to recipient object. </param>
 void createRecipient(struct ns1__Recipient * recipient)
@@ -44,9 +37,9 @@ void createRecipient(struct ns1__Recipient * recipient)
 
   if(l_sourceAnswer == 3)
   {
-    char * s_elminateFreeLine;            //when using is "scanf" and "fgets" occurs
-    assingString("", &s_elminateFreeLine);//a free line, this elminate this problem
-    free(s_elminateFreeLine);
+    char * s_eliminateFreeLine;            //when using is "scanf" and "fgets" occurs
+    assingString("", &s_eliminateFreeLine);//a free line, this elminate this problem
+    free(s_eliminateFreeLine);
 
   /// <assing the recipient name>
     assingString("Enter the recipient's name:", &m_recipient.recipient_USCOREname);
@@ -82,10 +75,12 @@ void createRecipient(struct ns1__Recipient * recipient)
   (*recipient) = m_recipient;
 }
 
+/// <summary>
+/// Releases memory from user data.
+/// </summary>
+/// <param name = "recipeint"> Pointer to recipient </param>
 void freeRecipient(struct ns1__Recipient * recipient)
 {
-  free(recipient->id);//id 
-  free(recipient->source);//inline, group or book
   free(recipient->recipient_USCOREname);//release name
   free(recipient->recipient_USCOREpost_USCOREcode);//release code
   free(recipient->recipient_USCOREcity);//release code
@@ -95,59 +90,4 @@ void freeRecipient(struct ns1__Recipient * recipient)
   free(recipient->recipient_USCOREflat_USCOREnumber);//release flat number
   free(recipient->recipient_USCOREcountry);//release country
   free(recipient->recipient_USCOREfax_USCOREnumber);//release fax number
-}
-
-/// <summary>
-/// Assing a string to proper answer.
-/// </summary>
-/// <param name = "answer"> Answer form main function. </param>
-/// <return> The proper answer. </return>
-void assingSource(const unsigned int answer, char ** stringOut)
-{
-  char * s_string = malloc(sizeof(char) * STRING_SIZE);
-  switch (answer)
-  {
-    case 1:
-      strcpy(s_string, "inline");
-    break;
-
-    case 2:
-      strcpy(s_string,"group");
-    break;
-
-    case 3:
-      strcpy(s_string,"book");
-    break;
-
-    default:
-      strcpy(s_string, "inline");
-  }
-  (*stringOut) = s_string;
-}
-
-/// <summary>
-/// Gets an information from user inputs.
-/// </summary>
-/// <param name = "consoleWrite"> An information prints in console </param>
-/// <return> user inputs </return>
-void assingString(const char * consoleWrite, char ** stringOut)
-{
-  (*stringOut) = malloc(sizeof(char) * STRING_SIZE);
-  printf("%s",consoleWrite);
-  fgets((*stringOut), STRING_SIZE - 1, stdin);
-  removeNewLine((*stringOut));
-}
-
-/// <summary>
-/// Removes a new line created by "fgets"
-/// </summary>
-/// <param name = "string"> String with new line created by "fgets". </param>
-void removeNewLine(char* string)
-{
-  unsigned int u_length = 0;
-  if((u_length = strlen(string)) > 0)
-  {
-    if(string[u_length-1] == '\n')
-      string[u_length-1] ='\0';
-  }
 }
